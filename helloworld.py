1cd34f6de8516c5d0efa54fb560d18595045fe05
carolinp <carolinp@live.unc.edu>
2014-01-15 13:28:02 -0500

    Add initial python file

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+#learning python commands
+
+print "Hello World!"
+
+print "My name is Caroline"
+
+print "Let's count some things"
+
+print "Boys", 25 + 30 / 6
+print "Girls", 100 - 25 * 3 % 4
+print "Let's count the the class:"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
